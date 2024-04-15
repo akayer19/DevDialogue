3012fc0a928cd1dfabdfee707345ed3a12102c56
akayer19 <akayeauto@gmail..com>
2024-04-15 14:50:18 -0700

    Set up Handlebars templates and styles

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 // Define routes
 router.get('/', (req, res) => {
-  res.send('Home Page');
+  // Render 'home' template using 'main' layout
+  res.render('home', {
+    layout: 'main' // This is usually the default and can be omitted if you have set 'main' as the default layout in the exphbs configuration
+  });
 });
 
 // more routes can be added here
